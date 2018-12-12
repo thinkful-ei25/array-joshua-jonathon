2be5e052c4de7ef77ddc94111c3e04ca48da02c1
Joshua Adams <joshadams554@gmail.com>
2018-12-12 11:53:16 -0500

    implimented array

--- thinkarray.js
+++ thinkarray.js
@@ -1,9 +1,74 @@
 'use strict';
-import memory from './memory';
-require(memory);
+const Memory = require('./memory');
+const memory = new Memory();
 class Array {
   constructor(){
     this.length = 0;
+    this._capacity = 0;
     this.ptr = memory.allocate(this.length);
   }
+
+  push(value) {
+    if(this.length > this._capacity){
+      this._resize((this.length ++) * Array.SIZE_RATIO);
+    }
+    
+    memory.set(this.ptr + this.length, value);
+    this.length ++;
+
+  }
+
+  _resize(val){
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(val);
+    if(this.ptr === null){
+      throw new Error('Out of memory');
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  get(index){
+    if(index < 0 || index > this.length){
+      throw new Error('Index Error');
+    }
+    return memory.get(this.ptr + index);
+  }
+
+  pop(){
+    if(this.length === 0){
+      throw new Error('Index Error');
+    }
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length --;
+    return value;
+  }
+  insert(index, value){
+    if(index < 0 || index >= this.length){
+      throw new Error('Index Error');
+    }
+    if(this.length >= this._capacity){
+      this._resize((this.length +1 ) * Array.SIZE_RATIO);
+    }
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length ++;
+  }
+
+  remove(index){
+    if(index < 0 || index >= this.length){
+      throw new Error('Index Error');
+    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length = index -1);
+    this.length--;
+  }
+  
 }
+
+Array.SIZE_RATIO = 3;
+
+const arr = new Array();
+arr.push(78);
+console.log(arr.pop());
+
